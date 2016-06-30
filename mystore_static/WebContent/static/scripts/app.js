f5354bdbe72f0196baec50bcdae8cfe48e4bc953
Ruchika Sharma <sharma.ruchika175@gmail.com>
2016-06-29 22:42:41 -0400

    Initial commit | Added homepage related files

---
+++ mystore_static/WebContent/static/scripts/app.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function() {
+	$(".g-close").on("click", function(e) {
+		e.preventDefault();
+		$(".top-line").hide();
+	});
+    $("#search-btn").on("click", function(){
+    	var data = $(".searchbox").val();
+    	var dataJson = {query : data};
+    	console.log(dataJson);
+        /*$.post("/abc", dataJson,function(a,b,c){
+	    	console.log(a);
+	    	console.log(b);
+	    	console.log(c);
+	    });*/	
+    });
+    $(".content").on("mouseover mouseout", function(){ 
+    	$(this).toggleClass("expanded");
+    	//$(this).find(".dropdown-list").toggle();
+	});
+       
+    $("#total").on("click", function(){
+    	var input1 = parseInt( $("#nmbr1").val());
+    	var input2 = parseInt( $("#nmbr2").val());
+    	var output = input1 + input2;
+    	$("#results").text(output);
+    });
+    
+    $("#total1").on("click", function(){
+    	addnumber1();
+    });
+    
+    function addnumber1(){
+    	var input3 = parseInt(document.getElementById("nmbr3").value);
+    	var input4 = parseInt(document.getElementById("nmbr4").value);
+    	var output1 = input3 + input4;
+    	document.getElementById("results1").innerHTML = output1;
+    }
+	
+	window.setInterval(runCarousel, 5000);
+	    var liWidth = $(".jcarousel ul li:first-child").width();
+	    var liCount = $(".jcarousel ul li").length;
+	    $(".jcarousel ul").width(liWidth * liCount);
+	
+	function runCarousel() {
+		//var selectedLI = $(".jcarousel li.selected-item");
+		//console.log(selectedItem);
+		/*if (selectedItem.length <= 0) {
+			$(".jcarousel ul li:first-child").addClass("selected-item");
+		} else {
+			
+		}*/
+		var carouselUL = $(".jcarousel ul");
+		var currentleft = Number.parseInt(carouselUL.css("left"));
+		var newLeft = 0;
+		var liWidth = $(".jcarousel ul li:first-child").width();
+		var liCount = $(".jcarousel ul li").length;
+		if (currentleft > - (liWidth * (liCount-1))) {
+			newLeft = -(Math.abs(currentleft) + liWidth);
+			}
+		console.log(currentleft +" : " + liWidth+ ":"+newLeft);
+		carouselUL.animate({left: newLeft+"px"});
+		//carouselUL.append($(".jcarousel ul li:first-child"));
+		//carouselUL.prepend($(".jcarousel ul"));
+		
+		
+	    
+	}
+}); 
